8df2315d8586961285de2a82918b2297680add08
student <zgomez4@miners.utep.edu>
2022-05-04 21:42:53 -0600

    made modifications

--- toy/draw_shapes.c
+++ toy/draw_shapes.c
@@ -1,5 +1,13 @@
 #include "draw_shapes.h"    
 #include "switches.h"
+#include "buzzer.h"
+#include "music.h"
+
+
+int period = 500;
+char songT = 0;
+
+
 
 // axis zero for col, axis 1 for row
 short drawPos[2] = {10,10}, controlPos[2] = {10,10};
@@ -11,7 +19,8 @@ u_int controlFontColor = COLOR_GREEN;
 
 // Color of the shape.
 unsigned int SHAPE_COLOR = COLOR_DARK_VIOLE;
-
+unsigned int SHAPE_COLOR2 = COLOR_ROYAL_BLUE;
+unsigned int SHAPE_COLOR3 = COLOR_DARK_OLIVE_GREEN;
 void update_shape()
 {
   static unsigned char row = screenHeight / 2, col = screenWidth / 2;
@@ -37,37 +46,69 @@ void update_shape()
     drawPixel(endCol+3, row+2, SHAPE_COLOR);
     drawPixel(endCol+3, row-3, SHAPE_COLOR);
     drawPixel(endCol+3, row+3, SHAPE_COLOR);
+    
 
-    // draw triangles on horizontal way 
-    fillRectangle(endCol+30, row*step, width, 1, COLOR_BLACK);
+    // Second shape 
+   fillRectangle(endCol, row+11-step, width, 1, SHAPE_COLOR2);
+   fillRectangle(endCol, row+11+step, width, 1, SHAPE_COLOR2);
+   drawPixel(endCol+10, row-7, SHAPE_COLOR2); 
+   drawPixel(endCol+10, row+7, SHAPE_COLOR2);
+   drawPixel(endCol+11, row+8, SHAPE_COLOR2);
+   drawPixel(endCol+11, row-8, SHAPE_COLOR2);
+   drawPixel(endCol+12, row+9, SHAPE_COLOR2);
+   drawPixel(endCol+12, row-9, SHAPE_COLOR2);
+   drawPixel(endCol+13, row+10, SHAPE_COLOR2);
+
+
+   // Third shape
+   fillRectangle(endCol, row+12-step, width, 1, SHAPE_COLOR3);
+   fillRectangle(endCol, row+12+step, width, 1, SHAPE_COLOR3);
+   drawPixel(endCol+14, row-12, SHAPE_COLOR3); 
+   drawPixel(endCol+14, row+12, SHAPE_COLOR3);
+   drawPixel(endCol+15, row+13, SHAPE_COLOR3);
+   drawPixel(endCol+15, row-14, SHAPE_COLOR3);
+   drawPixel(endCol+16, row+14, SHAPE_COLOR3);
+   drawPixel(endCol+16, row-15, SHAPE_COLOR3);
+   drawPixel(endCol+17, row+15, SHAPE_COLOR3);
    
+   
+   
+   // draw rectrangles on horizontal way 
+   // fillRectangle(endCol+30, row*step, width, 1, COLOR_BLACK);
     /* Make shadow of hot pink color to the violet shape   (violet is the original color of the shape) */
     
     if (switches & SW1)
     { // first button
       SHAPE_COLOR = COLOR_HOT_PINK;
-  
+      SHAPE_COLOR2 = COLOR_GOLD;
+      SHAPE_COLOR3 = COLOR_VIOLET;
+      
+       buzzer_set_period(0);
     }
         /* Make shadow of gold color to the violet shape   (violet is the original color of the shape) */
     // second button.
     if (switches & SW2)
     {
-      SHAPE_COLOR = COLOR_GOLD;
-     
+      SHAPE_COLOR = COLOR_BLUE;
+      SHAPE_COLOR2 = COLOR_GREEN;
+      SHAPE_COLOR3 = COLOR_RED;
+      buzzer_set_period(0);
     }
         /* Make shadow of aquamarine color to the violet shape   (violet is the original color of the shape) */
     // third button.
     if (switches & SW3)
     {
       SHAPE_COLOR = COLOR_AQUAMARINE;
-     
+      SHAPE_COLOR2 = COLOR_MAGENTA;
+      SHAPE_COLOR3 = COLOR_VIOLET;
+      buzzer_set_period(0);
     }
         /* Make shadow of black color to the violet shape   (violet is the original color of the shape) */
     // four button.
     if (switches & SW4)
     {
-      
-       clearScreen(COLOR_RED);
+      musicS();
+
     }
     step ++;
     
@@ -77,11 +118,11 @@ void update_shape()
 
     // move the shape to the left
     col -= colStep;
+
     
-    clearScreen(COLOR_GOLDENROD); // clear the screen with color goldenrod
-    clearScreen(COLOR_TAN);     // clear the screen with color tan
+    
+    clearScreen(COLOR_BLACK); // clear the screen with color black.
+    // clearScreen(COLOR_TAN);     // clear the screen with color tan
      step = 0;
   }
 }
-
-
